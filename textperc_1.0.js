33dcb2b3c78ac5c1e0dca568ec71515ce138fd46
Josh Comeau <joshwcomeau@gmail.com>
2014-07-09 19:39:47 -0400

    Added a debug function. Did a bit of tidy refactoring.

--- textperc_1.0.js
+++ textperc_1.0.js
@@ -1,26 +1,31 @@
 (function ( $ ) {
+
+
   $.fn.textPerc = function( options ) {
 
     // Let's set our default settings
-    var 
-    settings = $.extend({
-        percentage: 100,
-        textAlign: "center"
-    }, options );
-
+    var settings = $.extend({}, this.defaults, options );
 
     return this.each(function() {
-      // Our first job is to shrink the text down to mini-size. 
+      $element = $(this);
+
+      // Testing function.
+      debug( this );
+
+      // First, we set the text element (whether it's an h2 or a p or a div) to display: inline;.
+      // This is done so that we can find out how much width the letters themselves take up.
+      $element.css("display", "inline");
+
+      // Next, we shrink the text down to mini-size. 
       // This is done because when we retrieve the header's width, we want to make sure it fits on 1 line.
-      var 
-      starting_size = 9;
-      $(this).css("font-size", starting_size);
+      var starting_size = 9;
+      $element.css("font-size", starting_size);
 
       // It's variable time! Let's figure out who the major players are.
       var
-      container         = $(this).parent(),
+      container         = $element.parent(),
       container_width   = container.width(),
-      text_width        = $(this).width(),
+      text_width        = $element.width(),
       text_width_ratio  = text_width / container_width;
 
       // Data gathered, we now compute our new font size. 
@@ -28,8 +33,22 @@
       var
       new_font_size     = (starting_size / text_width_ratio) - 1;
 
-      return $(this).css("font-size", new_font_size);
+      $element.css("font-size", new_font_size);
     });
-    
   }
+
+  // Our debug function. For internal testing purposes only.
+  function debug( obj ) {
+    console.log( "The object is:" );
+    console.log( obj );
+    console.log( "-------------" );
+    console.log("There are " + obj.length + " item(s) selected with textPerc.")
+  }
+
+  $.fn.textPerc.defaults = {
+    percentage: 100,
+    textAlign: "center"
+  }
+
+
 }( jQuery ));
