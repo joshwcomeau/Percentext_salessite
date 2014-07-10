a37c335af59d9f9a035675817040fafcbded60ce
Josh Comeau <joshwcomeau@gmail.com>
2014-07-09 21:22:11 -0400

    Done for the night :smile_cat:

--- textperc_1.0.js
+++ textperc_1.0.js
@@ -4,7 +4,7 @@
   $.fn.textPerc = function( options ) {
 
     // Let's set our default settings
-    var settings = $.extend({}, this.defaults, options );
+    var settings = $.extend({}, $.fn.textPerc.defaults, options );
 
     return this.each(function() {
       var
@@ -14,7 +14,7 @@
       
 
       // Preparation is key.
-      setup( $elem, starting_size );      
+      setup( $elem, starting_size, settings );      
 
       // It's variable time! Let's figure out who the major players are.
       var
@@ -24,13 +24,12 @@
       text_width_ratio  = text_width / container_width;
 
 
-
       // Time to do our thing. This happens in two main parts. 
 
       // Part I: Broad Strokes.
       // We do some math to get what ought to be the perfect font size. This will work most times.
       var broad_font_size = broad_strokes( $elem, starting_size, text_width_ratio);
-      $elem.css("font-size", broad_font_size);
+      set_font(broad_font_size);  
 
       // Part II: Incremental Increases.
       // There are times where it might choose a font size that is a little too small.
@@ -39,40 +38,33 @@
       starting_height      = $elem.height(),
       increasing_font_size = broad_font_size;
 
-      console.log("Our initial font size is " + increasing_font_size);
-      console.log("Our 'while' condition is that " + $elem.height() + " must be less than " + starting_height*2 + " (which is " + starting_height + " times 2)");
-
       while ( $elem.height() < starting_height * 2 ) {
         increasing_font_size++;
-        $elem.css("font-size", increasing_font_size);
-        console.log(increasing_font_size);
-        console.log("height is " + $elem.height() );
+        set_font(increasing_font_size);
       }
-
       // We've gone one step too far. Let's undo that last iteration and call it a day!
       increasing_font_size--;
-      $elem.css("font-size", increasing_font_size);
+      set_font(increasing_font_size);
 
-      // Gotta wash some dishes
+
+      // Gotta brush your teeth before bed.
       cleanup( $elem );
-
-
-
-      // Testing function.
       debug( this );
 
     });
   }
 
   // Our setup function. Does a few quick things to get our text ready.
-  function setup( $elem, starting_size ) {
-    // First, we set the text element (whether it's an h2 or a p or a div) to display: inline;.
-    // This is done so that we can find out how much width the letters themselves take up.
-    $elem.css("display", "inline");
+  function setup( $elem, starting_size, settings ) {
+    $elem.css({
+      display:    "inline",
+      fontSize:   starting_size,
+      textAlign:  settings.alignment
+    });
+  }
 
-    // Next, we shrink the text down to mini-size. 
-    // This is done because when we retrieve the header's width, we want to make sure it fits on 1 line.
-    $elem.css("font-size", starting_size);
+  function set_font(font_size) {
+    $elem.css("font-size", font_size);
   }
 
   // Does some quick and dirty cross multiplication to get a good starting-point font size.
@@ -110,7 +102,7 @@
 
   $.fn.textPerc.defaults = {
     percentage: 100,
-    textAlign: "center"
+    alignment: "left"
   }
 
 
